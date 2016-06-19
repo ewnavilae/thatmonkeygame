01169332f07efa814377e08dea4f1bd259cc9e68
Ewna Vilae <ewnavilae@gmail.com>
2016-06-19 22:28:26 +0100

    Updated rounding helper to handle errors

--- helpers/roundTo.js
+++ helpers/roundTo.js
@@ -1,4 +1,9 @@
 import numeral from "numeral"
-export default ( number, points ) =>
-  numeral( points > 0 ? Math.round( number * ( 10 ** points ) ) / 10 ** points : Math.round( number ) )
-  .format( "0.[00]a" ).toUpperCase()
+export default ( number, points ) => {
+  try {
+    return numeral( points > 0 ? Math.round( number * ( 10 ** points ) ) / 10 ** points : Math.round( number ) )
+    .format( "0.[00]a" ).toUpperCase()
+  } catch ( e ) {
+    console.log( e )
+  }
+}
